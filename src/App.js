31a3218365161b630eca48e4c0b02c538a3dce24
Louie Cabral <louiealdrin25@gmail.com>
2022-04-27 18:39:42 +0800

    HTML Structure Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import './App.scss';
+import "./App.scss";
 function App() {
   return (
     <div className="App">
-     <h1>helloworld</h1>
+      <div className="container">
+        <div className="weather-form">
+          <form action="">
+            <div className="btn-con">
+              <input
+                type="text"
+                className="txt-searchCountry"
+                placeholder="Search City"
+                value=""
+                onChange=""
+              />
+              <input
+                type="submit"
+                className="btn-searchCountry"
+                value="Search"
+              />
+            </div>
+          </form>
+        </div>
+
+        <div className="con-status">
+          <div className="weather-search">
+            <h1>Searched City</h1>
+            <ul>
+              <li>City:</li>
+              <li>Region:</li>
+              <li>Country:</li>
+            </ul>
+          </div>
+
+          <div className="weather-stat">
+            <h1>Current Conditions</h1>
+            <ul>
+              <li>Feels Like:</li>
+              <li>Humidity:</li>
+              <li>Wind:</li>
+              <li>Sunrise:</li>
+              <li>Sunset:</li>
+              <li>Pressure:</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
